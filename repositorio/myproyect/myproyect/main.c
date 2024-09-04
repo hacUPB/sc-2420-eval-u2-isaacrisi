97e0f665050c8602e083f99405a4eede58324c73
isaacrisi <isaacpinedamunera@gmail.com>
2024-09-04 16:39:55 -0500

    creating window

--- repositorio/myproyect/myproyect/main.c
+++ repositorio/myproyect/myproyect/main.c
@@ -1,9 +1,106 @@
 #include <stdio.h>
 #include <SDL.h>
+#include "Constants.h"
+
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int game_is_running = NULL;
+
+
+int initialize_window(void)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		fprintf(stderr, "Error initializing SDL \n");
+		return FALSE;
+
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT, 
+		SDL_WINDOW_BORDERLESS
+		);
+	if (!window)
+	{
+		fprintf(stderr, "Error creating SDL window \n");
+		return FALSE;
+
+	}
+
+	renderer = SDL_CreateRenderer(
+		window,
+		-1,
+		0
+	);
+
+	if (!renderer)
+	{
+		fprintf(stderr, "Error creating renderer \n");
+		return FALSE;
+
+	}
+
+	return TRUE;
+}
+
+void process_input()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		game_is_running = FALSE;
+		break;
+	case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE)
+		{
+			game_is_running = FALSE;
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+void setup()
+{
+
+}
+void update()
+{
+	// to do:
+}
+void render()
+{
+	// to do:
+}
+void destroy_window()
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
 int main(int argc, char* args[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	printf("Hola mundo");
+	game_is_running = initialize_window();
+
+	setup();
+
+	while (game_is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+
 	return 0;
 }
